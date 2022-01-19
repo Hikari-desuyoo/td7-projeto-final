98f24ecff96bef248e69df655beff8c3f7a80ba6
Hikari-desuyoo <hikaridesuyoo@gmail.com>
2022-01-19 13:12:45 -0300

    (WIP)Adicionado postgres no projeto, testes falhando

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_16_195801) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorited_hirers", force: :cascade do |t|
     t.integer "hirer_id", null: false
     t.integer "worker_id", null: false
